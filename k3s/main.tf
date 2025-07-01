9a2dd00c6be0b1a26923c249964fdf207c2e1274
alexrsit <alex@rosta.dev>
2025-07-01 16:09:09 +0200

    postfix

--- k3s/main.tf
+++ k3s/main.tf
@@ -14,13 +14,39 @@ data "kubernetes_service" "nginx_ingress" {
   }
 }
 
+data "akeyless_static_secret" "armory_secrets" {
+  path = "/armory/blizzard"
+}
+
+data "akeyless_static_secret" "cloudflare_secrets" {
+  path = "/cloudflare/cloudflare"
+
+}
+
+data "akeyless_static_secret" "postfix_secrets" {
+  path = "/postfix/postfix"
+
+}
+
+locals {
+  armory_secrets = jsondecode(data.akeyless_static_secret.armory_secrets.value)
+}
+
+locals {
+  cloudflare_secrets = jsondecode(data.akeyless_static_secret.cloudflare_secrets.value)
+}
+
+locals {
+  postfix_secrets = jsondecode(data.akeyless_static_secret.postfix_secrets.value)
+}
+
 module "armory_dns" {
   source             = "../modules/dns"
-  cloudflare_email   = var.cloudflare_email
-  cloudflare_api_key = var.cloudflare_api_key
-  cloudflare_zone_id = var.cloudflare_zone_id
+  cloudflare_email   = local.cloudflare_secrets.cloudflare_email
+  cloudflare_api_key = local.cloudflare_secrets.cloudflare_api_key
+  cloudflare_zone_id = local.cloudflare_secrets.cloudflare_zone_id
   content            = data.kubernetes_service.nginx_ingress.status[0].load_balancer[0].ingress[0].ip
-  name               = "armory.${var.domain}"
+  name               = "armory.${local.cloudflare_secrets.domain}"
 }
 
 resource "kubernetes_manifest" "app-armory" {
@@ -31,19 +57,36 @@ resource "kubernetes_manifest" "monitoring-grafana" {
   manifest = yamldecode(file("../gitops/argocd/monitoring-grafana.yaml"))
 }
 
+resource "kubernetes_manifest" "app-postfix" {
+  manifest = yamldecode(file("../gitops/argocd/app-postfix.yaml"))
+}
+
 resource "kubernetes_secret" "app-armory-secret" {
   metadata {
     name      = "blizz-clientid"
     namespace = "armory"
   }
   data = {
-    "CLIENT_ID"              = var.blizz_clientid
-    "CLIENT_SECRET"          = var.blizz_clientsecret
-    "WARCRAFTLOGS_API_TOKEN" = var.warcraftlogs_api_token
-    "REDIS_ADDR"             = var.redis_addr
-    "REDIS_PASSWORD"         = var.redis_password
-    "REDIS_DB"               = var.redis_db
+    "CLIENT_ID"              = local.armory_secrets.client_id
+    "CLIENT_SECRET"          = local.armory_secrets.client_secret
+    "WARCRAFTLOGS_API_TOKEN" = local.armory_secrets.warcraftlogs_token
+    "REDIS_ADDR"             = local.armory_secrets.redis_addr
+    "REDIS_PASSWORD"         = local.armory_secrets.redis_password
+    "REDIS_DB"               = local.armory_secrets.redis_db
   }
   depends_on = [kubernetes_manifest.app-armory]
+
+}
+
+resource "kubernetes_secret" "app-postfix-secret" {
+  metadata {
+    name      = "postfix-relayhost"
+    namespace = "postfix"
+  }
+  data = {
+    "username" = local.postfix_secrets.postfix_username
+    "password" = local.postfix_secrets.postfix_password
+  }
+  depends_on = [kubernetes_manifest.app-postfix]
 
 }
--- k3s/variables.tf
+++ k3s/variables.tf
@@ -1,80 +1,21 @@
-variable "domain" {
-  description = "Base domain for ingress resources"
-  type        = string
-  default     = "rosta.dev"
-}
-
 variable "email" {
   description = "Email for Let's Encrypt"
   type        = string
   default     = "alex@rosta.dev"
 }
 
-provider "cloudflare" {
-  email   = var.cloudflare_email
-  api_key = var.cloudflare_api_key
-}
 
-variable "cloudflare_email" {
-  description = "Cloudflare account email"
-  type        = string
-  sensitive   = true
 
-}
-
-variable "cloudflare_api_key" {
-  description = "Cloudflare API key"
+variable "akeyless_access_id" {
+  description = "Akeyless access ID"
   type        = string
   sensitive   = true
 }
 
-variable "cloudflare_zone_id" {
-  description = "Cloudflare zone ID"
+variable "akeyless_access_key" {
+  description = "Akeyless access key"
   type        = string
   sensitive   = true
 }
-
-variable "blizz_clientid" {
-  description = "Blizzard client ID"
-  type        = string
-  sensitive   = true
-
-}
-
-variable "blizz_clientsecret" {
-  description = "Blizzard client secret"
-  type        = string
-  sensitive   = true
-
-}
-
-variable "warcraftlogs_api_token" {
-  description = "WarcraftLogs API token"
-  type        = string
-  sensitive   = true
-
-}
-
-variable "redis_addr" {
-  description = "Redis address"
-  type        = string
-  sensitive   = true
-
-}
-
-variable "redis_password" {
-  description = "Redis password"
-  type        = string
-  sensitive   = true
-
-}
-
-variable "redis_db" {
-  description = "Redis database number"
-  type        = string
-  default     = "0"
-
-}
-
 
 
